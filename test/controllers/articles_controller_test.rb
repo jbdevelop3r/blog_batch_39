3bf154e52f831c9fc3935fe95c6b54ef3f769ec3
jbdevelop3r <justinebanogon14@gmail.com>
2025-07-05 10:30:28 +0800

    added testing

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get articles_path
+
+    assert_response :success
+  end
+
+  test "should get mew" do
+    get new_article_path
+
+    assert_response :success
+  end
+
+  test "should create an article with valid params" do
+    post articles_path, params: { article: { title: "test title", body: "test body" } }
+
+    assert_response :redirect
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save an article without a title" do
+    article = Article.new(body: "test body")
+    assert_not article.save
+  end
+
+  test "should not save an article without a body" do
+    article = Article.new(title: "test title")
+    assert_not article.save
+  end
+
+  test "should not save an article with < 5 chars" do
+    article = Article.new(title: "test title", body: "test")
+    assert_not article.save
+  end
+
+  test "should save an article with valid params" do
+    article = Article.new(title: "test title", body: "test body")
+    assert article.save
+  end
 end
